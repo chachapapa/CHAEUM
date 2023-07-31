e6d541a091fe0c7de77968b6f21e503892d63aec
Jaewoong Jeong <jjwoong1733@gmail.com>
2023-07-31 16:35:07 +0900

    :sparkles: Feat : #170 색상 스펙트럼 >> - material tailwind 내 Radio 컴포넌트 활용하여 색상을 선택하는 버튼 리스트 제작 >> - 이후 해당 색상코드를 state에 저장하여 백에 전해주는 클릭이벤트 제작 필요.

--- FE/chaeum-frontend/src/Components/ColorContainer.tsx
+++ FE/chaeum-frontend/src/Components/ColorContainer.tsx
@@ -1,18 +1,40 @@
-import React from 'react';
+import { Radio } from '@material-tailwind/react';
+import React, { useState } from 'react';
 
 type Props = {
   color: string;
+  hoverColor: string;
 };
 
 //상위 컴포넌트에서 매핑하는 형태
-{/* <div className="flex">
-  {arr.map((container, key) => (
-    <ColorContainer key={key} color={container} />
-  ))}
-</div>; */}
+{
+  /* <div className="flex w-[360px] mt-5 overflow-auto">
+                {arr.map((container, key) => (
+                  <ColorContainer
+                    key={key}
+                    color={container.color}
+                    hoverColor={container.hoverColor}
+                  />
+                ))}
+              </div> */
+}
 
 const ColorContainer = (props: Props) => {
-  return <div className={`w-6 h-6 mr-2 ${props.color}`}></div>;
+  const [isClicked, setIsClicked] = useState<boolean>(false);
+
+  const onButtonClick = () => {
+    setIsClicked(!isClicked);
+  };
+
+  return (
+
+    <Radio
+      name="type"
+      ripple={false}
+      icon={<i className="fa-solid fa-check text-white text-sm"></i>}
+      className={`${props.color} ${props.hoverColor} p-0 transition-all hover:before:opacity-0 border-0 rounded-lg`}
+    />
+  );
 };
 
 export default ColorContainer;
