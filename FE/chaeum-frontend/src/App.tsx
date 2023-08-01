2721c09e98cfdd5f372a5c5c40f29b648095fc22
kjy6135@naver.com <kjy6135@naver.com>
2023-08-01 16:48:46 +0900

    :sparkles: Feat : #256 Routing
    - App.tsx 에 views 디렉토리에 있는
    - 여러 페이지 import 후 Routes 태그 내부에 정의

--- FE/chaeum-frontend/src/App.tsx
+++ FE/chaeum-frontend/src/App.tsx
@@ -1,11 +1,56 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import ArticleCard from './components/feed/ArticleCard';
 import EnterancePage from './views/EnterancePage';
+import SignupPage from './views/SignupPage';
+import MainPage from './views/MainPage';
+import ActivePage from './views/ActivePage';
+import ResultPage from './views/ResultPage';
+import FeedPage from './views/FeedPage';
+import FeedWritePage from './views/FeedWritePage';
+import FeedDetailPage from './views/FeedDetailPage';
+import ChatPage from './views/ChatPage';
+import ChatInitPage from './views/ChatInitPage';
+import ChatRoomPage from './views/ChatRoomPage';
+import ProfilePage from './views/ProfilePage';
+
+import { Navigate, Route, Routes, useLocation } from 'react-router-dom';
 
 function App() {
+  const [isLogedin, setIsLogedin] = useState(false);
+
+  const location = useLocation();
+  useEffect(() => {
+    if (sessionStorage.getItem('loginUser') !== null) {
+      setIsLogedin(true);
+    } else {
+      setIsLogedin(false);
+    }
+  }, [location.pathname]);
+
   return (
     <div className="App">
+      <Routes>
+        <Route path="/entrance" element={<EnterancePage />}></Route>
+        <Route path="/signup" element={<SignupPage />}></Route>
+        <Route path="/main" element={<MainPage />}></Route>
+        <Route
+          path="/"
+          element={isLogedin ? <MainPage /> : <Navigate to="/enterance" />}
+        />
+        <Route path="/active" element={<ActivePage />}>
+          <Route path="result" element={<ResultPage />}></Route>
+        </Route>
+        <Route path="/feed" element={<FeedPage />}>
+          <Route path="write" element={<FeedWritePage />}></Route>
+          <Route path="detail" element={<FeedDetailPage />}></Route>
+        </Route>
+        <Route path="/chat" element={<ChatPage />}>
+          <Route path="init" element={<ChatInitPage />}></Route>
+          <Route path=":userId" element={<ChatRoomPage />}></Route>
+        </Route>
+        <Route path="/profile/:userId" element={<ProfilePage />}></Route>
+      </Routes>
       <EnterancePage />
     </div>
   );
