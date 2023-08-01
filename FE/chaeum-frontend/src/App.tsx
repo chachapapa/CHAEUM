a099c532f63ce69edd63ba650ecb30b73f05da3e
unknown <khappy517@gmail.com>
2023-08-01 15:22:52 +0900

    Rename : #289 컴포넌트 디렉토리 수정
    - 폴더 명 수정 : Views to views

--- FE/chaeum-frontend/src/App.tsx
+++ FE/chaeum-frontend/src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import ArticleCard from './components/feed/ArticleCard';
 import EnterancePage from './views/EnterancePage';
+
 function App() {
   return (
     <div className="App">
