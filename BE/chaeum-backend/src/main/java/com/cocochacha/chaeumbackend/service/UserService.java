9af4a8ccb264b89a1bab56e8ba701752f07e03cc
wjdalsdud37 <wjdalsdud37@naver.com>
2023-08-02 11:08:34 +0900

    :sparkles: Feat : #142 [BE] 카카오 로그인을 통한 회원가입, 로그인
    - OAuthLoginController.java를 통해 /oauth/login/kakao 요청을 처리

--- BE/chaeum-backend/src/main/java/com/cocochacha/chaeumbackend/service/UserService.java
+++ BE/chaeum-backend/src/main/java/com/cocochacha/chaeumbackend/service/UserService.java
@@ -1,10 +1,8 @@
 package com.cocochacha.chaeumbackend.service;
 
 import com.cocochacha.chaeumbackend.domain.User;
-import com.cocochacha.chaeumbackend.dto.AddUserRequest;
 import com.cocochacha.chaeumbackend.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
@@ -13,22 +11,39 @@ public class UserService {
 
     private final UserRepository userRepository;
 
-    public Long save(AddUserRequest dto) {
-        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-
-        return userRepository.save(User.builder()
-                .email(dto.getEmail())
-                .password(encoder.encode(dto.getPassword()))
-                .build()).getId();
-    }
-
+    /**
+     * 주어진 사용자 ID에 해당하는 사용자를 검색합니다.
+     *
+     * @param userId 검색하고자 하는 사용자의 ID(Long 타입)
+     * @return 검색된 사용자 객체(User)
+     * @throws IllegalArgumentException 주어진 사용자 ID에 해당하는 사용자가 없는 경우 예외 발생
+     */
     public User findById(Long userId) {
         return userRepository.findById(userId)
                 .orElseThrow(() -> new IllegalArgumentException("Unexpected user"));
     }
 
+    /**
+     * 주어진 이메일에 해당하는 사용자를 검색합니다.
+     *
+     * @param email 검색하고자 하는 사용자의 이메일 주소
+     * @return 검색된 사용자 객체(User)
+     * @throws IllegalArgumentException 주어진 이메일에 해당하는 사용자가 없는 경우 예외 발생
+     */
     public User findByEmail(String email) {
         return userRepository.findByEmail(email)
                 .orElseThrow(() -> new IllegalArgumentException("Unexpected user"));
+    }
+
+    /**
+     * 주어진 닉네임에 해당하는 사용자를 검색합니다.
+     *
+     * @param nickname 검색하고자 하는 사용자의 닉네임
+     * @return 검색된 사용자 객체
+     * @throws IllegalArgumentException 주어진 사용자 ID에 해당하는 사용자가 없는 경우 예외 발생
+     */
+    public User findByNickname(String nickname) {
+        return userRepository.findByNickname(nickname)
+                .orElse(null);
     }
 }
