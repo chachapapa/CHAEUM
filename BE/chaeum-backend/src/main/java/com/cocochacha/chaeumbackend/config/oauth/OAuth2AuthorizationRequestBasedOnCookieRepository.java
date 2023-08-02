4b0838e4f23bdc78575f28b919bb5fd4246e886a
wjdalsdud37 <wjdalsdud37@naver.com>
2023-08-02 11:01:58 +0900

    :sparkles: Feat : #139 [BE] OAuth 사용을 위한 Config 작성
    - OAuth2AuthorizationRequestBasedOnCookieRepository.java로 인증 요청 관련 상태를 쿠키에 저장
    - OAuth2UserCustomService로 Spring Security의 DefaultOAuth2UserService 구현
    - application.yml에 관련 설정 수정

--- BE/chaeum-backend/src/main/java/com/cocochacha/chaeumbackend/config/oauth/OAuth2AuthorizationRequestBasedOnCookieRepository.java
+++ BE/chaeum-backend/src/main/java/com/cocochacha/chaeumbackend/config/oauth/OAuth2AuthorizationRequestBasedOnCookieRepository.java
@@ -8,9 +8,15 @@ import org.springframework.security.oauth2.client.web.AuthorizationRequestReposi
 import org.springframework.security.oauth2.core.endpoint.OAuth2AuthorizationRequest;
 import org.springframework.web.util.WebUtils;
 
+/**
+ * OAuth2에 필요한 정보를 세션이 아닌 쿠키에 저장하여 사용할 수 있도록 인증 요청과 관련된 상태를 저장하는 저장소입니다.
+ */
 public class OAuth2AuthorizationRequestBasedOnCookieRepository implements
         AuthorizationRequestRepository<OAuth2AuthorizationRequest> {
 
+    /**
+     * OAuth2 인증 요청을 저장하는 쿠키의 이름입니다.
+     */
     public final static String OAUTH2_AUTHORIZATION_REQUEST_COOKIE_NAME = "oauth2_auth_request";
     private final static int COOKIE_EXPIRE_SECONDS = 18000;
 
@@ -30,14 +36,22 @@ public class OAuth2AuthorizationRequestBasedOnCookieRepository implements
     public void saveAuthorizationRequest(OAuth2AuthorizationRequest authorizationRequest,
             HttpServletRequest request, HttpServletResponse response) {
         if (authorizationRequest == null) {
+            // 인증 요청 정보가 없으면 쿠키를 제거합니다.
             removeAuthorizationRequestCookies(request, response);
             return;
         }
 
+        // OAuth2 인증 요청 정보를 쿠키에 저장합니다.
         CookieUtil.addCookie(response, OAUTH2_AUTHORIZATION_REQUEST_COOKIE_NAME,
                 CookieUtil.serialize(authorizationRequest), COOKIE_EXPIRE_SECONDS);
     }
 
+    /**
+     * 쿠키에서 OAuth2 인증 요청과 관련된 정보를 제거합니다.
+     *
+     * @param request  HttpServletRequest 객체
+     * @param response HttpServletResponse 객체
+     */
     public void removeAuthorizationRequestCookies(HttpServletRequest request,
             HttpServletResponse response) {
         CookieUtil.deleteCookie(request, response, OAUTH2_AUTHORIZATION_REQUEST_COOKIE_NAME);
--- BE/chaeum-backend/src/main/java/com/cocochacha/chaeumbackend/config/oauth/OAuth2UserCustomService.java
+++ BE/chaeum-backend/src/main/java/com/cocochacha/chaeumbackend/config/oauth/OAuth2UserCustomService.java
@@ -10,12 +10,23 @@ import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
 
+/**
+ * OAuth2UserCustomService 클래스는 OAuth2 사용자 정보를 처리하기 위한 Spring Security의 DefaultOAuth2UserService의
+ * 커스텀 구현체입니다.
+ */
 @RequiredArgsConstructor
 @Service
 public class OAuth2UserCustomService extends DefaultOAuth2UserService {
 
     private final UserRepository userRepository;
 
+    /**
+     * OAuth2UserRequest를 기반으로 사용자 정보를 가져옵니다. 가져온 정보를 데이터베이스에 저장 또는 업데이트합니다.
+     *
+     * @param userRequest 사용자 정보를 요청하는 OAuth2UserRequest 객체
+     * @return 사용자 정보를 담고 있는 OAuth2User 객체
+     * @throws OAuth2AuthenticationException 인증에 관련된 오류가 발생한 경우 예외를 던집니다.
+     */
     @Override
     public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
         // 요청을 바탕으로 유저 정보를 담은 객체 반환
@@ -25,18 +36,29 @@ public class OAuth2UserCustomService extends DefaultOAuth2UserService {
         return user;
     }
 
-    // ❷ 유저가 있으면 업데이트, 없으면 유저 생성
+    /**
+     * OAuth2User 정보를 기반으로 데이터베이스에 사용자 정보를 저장하거나 업데이트합니다.
+     *
+     * @param oAuth2User 사용자 정보를 담고 있는 OAuth2User 객체
+     * @return 데이터베이스에 저장 또는 업데이트된 User 객체를 반환합니다.
+     */
     private User saveOrUpdate(OAuth2User oAuth2User) {
         Map<String, Object> attributes = oAuth2User.getAttributes();
+        Map<String, Object> kakaoAccount = (Map<String, Object>) attributes.get("kakao_account");
+        Map<String, Object> profile = (Map<String, Object>) kakaoAccount.get("profile");
 
-        String email = (String) attributes.get("email");
-        String name = (String) attributes.get("name");
+        Long id = (Long) attributes.get("id");
+        String email = (String) kakaoAccount.get("email");
+        String name = (String) profile.get("nickname");
+        String profileImageUrl = (String) profile.get("profile_image_url");
 
         User user = userRepository.findByEmail(email)
-                .map(entity -> entity.update(name))
                 .orElse(User.builder()
+                        .id(id)
                         .email(email)
                         .nickname(name)
+                        .profileImageUrl(profileImageUrl)
+                        .isRegistered(false)
                         .build());
 
         return userRepository.save(user);
