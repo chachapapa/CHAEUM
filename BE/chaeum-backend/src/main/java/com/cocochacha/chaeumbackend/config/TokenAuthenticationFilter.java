1501b52295de7e19877f68092a03fb826651870d
wjdalsdud37 <wjdalsdud37@naver.com>
2023-08-02 10:56:53 +0900

    :sparkles: Feat : #138 [BE] JWT 사용을 위한 Config 작성
    - JwtProperties.java에 JWT 관련 프로퍼티를 담고 있음
    - TokenProvider.java로 JWT 토큰 생성 및 검증
    - TokenAuthentication.java로 JWT 토큰을 사용하여 사용자를 인증
    - WebOAuthSecurityConfig.java로 웹 OAuth 보안 구성

--- BE/chaeum-backend/src/main/java/com/cocochacha/chaeumbackend/config/TokenAuthenticationFilter.java
+++ BE/chaeum-backend/src/main/java/com/cocochacha/chaeumbackend/config/TokenAuthenticationFilter.java
@@ -11,6 +11,9 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.OncePerRequestFilter;
 
+/**
+ * JWT 토큰을 사용하여 사용자를 인증하는 필터 클래스입니다.
+ */
 @RequiredArgsConstructor
 public class TokenAuthenticationFilter extends OncePerRequestFilter {
 
@@ -19,15 +22,27 @@ public class TokenAuthenticationFilter extends OncePerRequestFilter {
     private final static String HEADER_AUTHORIZATION = "Authorization";
     private final static String TOKEN_PREFIX = "Bearer ";
 
+    /**
+     * HTTP 요청을 필터링하여 JWT 토큰을 사용하여 사용자를 인증하고 인증 정보를 설정합니다. 만약 토큰이 유효한 경우, {@link
+     * SecurityContextHolder}에 인증 정보를 설정합니다.
+     *
+     * @param request     HttpServletRequest 객체
+     * @param response    HttpServletResponse 객체
+     * @param filterChain 다음 필터를 호출하기 위한 FilterChain 객체
+     * @throws ServletException 서블릿 예외
+     * @throws IOException      입출력 예외
+     */
     @Override
     protected void doFilterInternal(
             HttpServletRequest request,
             HttpServletResponse response,
             FilterChain filterChain) throws ServletException, IOException {
 
+        // 요청 헤더의 Authorization 키의 값 조회
         String authorizationHeader = request.getHeader(HEADER_AUTHORIZATION);
+        // 가져온 값에서 접두사 제거
         String token = getAccessToken(authorizationHeader);
-
+        // 가져온 토큰이 유효한지 확인하고, 유효한 때는 인증 정보를 설정
         if (tokenProvider.validToken(token)) {
             Authentication authentication = tokenProvider.getAuthentication(token);
             SecurityContextHolder.getContext().setAuthentication(authentication);
@@ -36,6 +51,12 @@ public class TokenAuthenticationFilter extends OncePerRequestFilter {
         filterChain.doFilter(request, response);
     }
 
+    /**
+     * Authorization 헤더에서 토큰을 추출합니다.
+     *
+     * @param authorizationHeader Authorization 헤더 값
+     * @return 추출된 토큰 문자열 (Bearer 접두사 제거)
+     */
     private String getAccessToken(String authorizationHeader) {
         if (authorizationHeader != null && authorizationHeader.startsWith(TOKEN_PREFIX)) {
             return authorizationHeader.substring(TOKEN_PREFIX.length());
