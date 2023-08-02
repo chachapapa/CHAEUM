e0a1cd3b310f12ecf790dfa861971457b56da239
wjdalsdud37 <wjdalsdud37@naver.com>
2023-08-02 15:04:37 +0900

    :sparkles: Feat : #141 [BE] 유저 정보 초기 설정
    - POST /api/user/me 요청 처리
    - 닉네임 중복 체크 후 닉네임 갱신

---
+++ BE/chaeum-backend/src/main/java/com/cocochacha/chaeumbackend/dto/MyInfoRequest.java
@@ -0,0 +1,13 @@
+package com.cocochacha.chaeumbackend.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class MyInfoRequest {
+
+    private String nickname;
+}
