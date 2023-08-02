ac09fb710c300c00a36d3854fc572ea0802262cf
wjdalsdud37 <wjdalsdud37@naver.com>
2023-08-02 15:01:51 +0900

    :sparkles: Feat : #144 [BE] 닉네임 중복체크 구현
    - /api/user/duplication-check 요청 처리
    - 닉네임이 중복되지 않는 경우 true, 중복되는 경우 false를 반환

--- BE/chaeum-backend/src/main/java/com/cocochacha/chaeumbackend/controller/UserApiController.java
+++ BE/chaeum-backend/src/main/java/com/cocochacha/chaeumbackend/controller/UserApiController.java
@@ -11,6 +11,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
@@ -42,5 +43,25 @@ public class UserApiController {
                 user.getProfileImageUrl(), user.getIsRegistered());
 
         return ResponseEntity.ok().body(myInfoResponse);
+    }
+
+    /**
+     * 주어진 닉네임의 중복 여부를 체크하는 API 엔드포인트입니다.
+     *
+     * @param nickname 검사할 닉네임
+     * @param request  HttpServletRequest 객체
+     * @param response HttpServletResponse 객체
+     * @return 닉네임이 중복되지 않을 때 true를, 중복될 때 false를 ResponseEntity로 응답합니다.
+     */
+    @GetMapping("duplication-check")
+    public ResponseEntity<Boolean> nicknameDuplicationCheck(@RequestParam String nickname,
+            HttpServletRequest request, HttpServletResponse response) {
+
+        // 주어진 닉네임이 중복되지 않을 경우 true, 중복될 경우 false를 응답
+        if (userService.findByNickname(nickname) != null) {
+            return ResponseEntity.ok().body(false);
+        }
+
+        return ResponseEntity.ok().body(true);
     }
 }
