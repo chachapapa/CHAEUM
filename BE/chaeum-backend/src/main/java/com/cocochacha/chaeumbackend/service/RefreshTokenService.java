6451ede8b56d4c0d83ced6b76973d4eaaa9668ff
wjdalsdud37 <wjdalsdud37@naver.com>
2023-08-02 15:11:10 +0900

    :ambulance: HOTFIX : [BE] 한글 인코딩 깨지는 행 제거

--- BE/chaeum-backend/src/main/java/com/cocochacha/chaeumbackend/service/RefreshTokenService.java
+++ BE/chaeum-backend/src/main/java/com/cocochacha/chaeumbackend/service/RefreshTokenService.java
@@ -11,13 +11,6 @@ public class RefreshTokenService {
 
     private final RefreshTokenRepository refreshTokenRepository;
 
-    /**
-     * 주어진 리프레시 토큰으로 데이터베이스에서 해당 RefreshToken을 검색합니다.
-     *
-     * @param refreshToken 검색하고자 하는 리프레시 토큰
-     * @return 검색된 RefreshToken 객체
-     * @throws IllegalArgumentException 주어진 리프레시 토큰으로 검색된 RefreshToken이 없는 경우 예외 발생
-     */
     public RefreshToken findByRefreshToken(String refreshToken) {
         return refreshTokenRepository.findByRefreshToken(refreshToken)
                 .orElseThrow(() -> new IllegalArgumentException("Unexpected token"));
