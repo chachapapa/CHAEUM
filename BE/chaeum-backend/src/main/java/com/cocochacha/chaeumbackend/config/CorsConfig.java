2bf79c12008aabefc1eef328c84491aa86ee8394
wjdalsdud37 <wjdalsdud37@naver.com>
2023-08-04 15:31:52 +0900

    :ambulance: Hotfix : Cors 설정 수정

--- BE/chaeum-backend/src/main/java/com/cocochacha/chaeumbackend/config/CorsConfig.java
+++ BE/chaeum-backend/src/main/java/com/cocochacha/chaeumbackend/config/CorsConfig.java
@@ -1,26 +1,26 @@
-package com.cocochacha.chaeumbackend.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-/**
- * CORS 설정을 관리하는 클래스입니다.
- */
-@Configuration
-public class CorsConfig implements WebMvcConfigurer {
-
-    /**
-     * CORS 설정을 추가합니다.
-     *
-     * @param registry CORS 설정 레지스트리
-     */
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-//                .allowedOriginPatterns("*")
-//                .allowedMethods("GET", "POST", "PUT", "DELETE")
-                .allowedOrigins("http://i9a810.p.ssafy.io:80", "http://localhost:3000",
-                        "http://i9a810.p.ssafy.io:8080", "http://localhost:8080");
-    }
-}
+//package com.cocochacha.chaeumbackend.config;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.CorsRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+///**
+// * CORS 설정을 관리하는 클래스입니다.
+// */
+//@Configuration
+//public class CorsConfig implements WebMvcConfigurer {
+//
+//    /**
+//     * CORS 설정을 추가합니다.
+//     *
+//     * @param registry CORS 설정 레지스트리
+//     */
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/**")
+////                .allowedOriginPatterns("*")
+////                .allowedMethods("GET", "POST", "PUT", "DELETE")
+//                .allowedOrigins("http://i9a810.p.ssafy.io:80", "http://localhost:3000",
+//                        "http://i9a810.p.ssafy.io:8080", "http://localhost:8080");
+//    }
+//}
