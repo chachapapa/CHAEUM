cdadc586e550a58b7cb9fa98a479b86e1df5611f
kjy6135@naver.com <kjy6135@naver.com>
2023-08-06 15:29:45 +0900

    :hammer: Refactor : #300 컴포넌트 추가 작업
    - StoryCard.tsx 수정
    - 자기소개 삭제 (공간상의 이유)
    - 닉네임 글자 제한 (한글 8글자)
    - 태그는 최대 3칸 지원
    - 타이머는 string이 아닌 number 값으로 넘겨서 active한 성격을 지니도록 수정

--- FE/chaeum-frontend/src/components/feed/StoryCard.tsx
+++ FE/chaeum-frontend/src/components/feed/StoryCard.tsx
@@ -1,36 +1,60 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import '../styles/activewave.css';
 import { ReactComponent as Active } from '../../assets/active_section.svg';
-import {
-  Card,
-  CardHeader,
-  CardBody,
-  Typography,
-  Avatar,
-} from '@material-tailwind/react';
 
 /*
   사용법
-  <StoryCard
-    nickname="chacha"
-    img="../chacha2.png"
-    ment="나는 산양이 될테야"
-    color="yellow"
-    tag="#클라이밍"
-    time="02:03:25"
-  ></StoryCard>
+  <StoryCard 
+    color='yellow' 
+    img='../chacha2.png' 
+    nickname='코코' 
+    tag={['tag1', 'tag2', 'tag3']} 
+    time={15}>
+  </StoryCard>
+
+  닉네임은 8글자(한글) 제한이 될 것 같습니다
+  
+  태그는 최대 3칸 지원하며(공간상의 문제)
+  
+  타이머는 컴포넌트 내에서 연산을 계속하는것 보다
+  "현재 시간 - 최초 활동시간" 을 number로 넘겨서
+  작동하게끔 로직을 구현했습니다.
 */
 
 type Props = {
   color: 'blue' | 'yellow' | 'lime' | 'navy' | 'gray';
   img: string;
   nickname: string;
-  ment: string;
-  tag: string;
-  time: string;
+  tag: string[];
+  // 렌더링 문제로 string이 아닌 number로 넘겨서 계산해야 합니다.
+  time: number;
 };
 
 const StoryCard = (props: Props) => {
+  const [time, setTime] = useState(props.time);
+
+  useEffect(() => {
+    // Update time every second
+    const interval = setInterval(() => {
+      setTime((prevTime) => prevTime + 100); // Increment time by 100 milliseconds (1 second)
+    }, 1000);
+
+    // Cleanup the interval on component unmount
+    return () => clearInterval(interval);
+  }, []);
+
+  // Hours calculation
+  const hours = Math.floor(time / 360000);
+
+  // Minutes calculation
+  const minutes = Math.floor((time % 360000) / 6000);
+
+  // Seconds calculation
+  const seconds = Math.floor((time % 6000) / 100);
+
+  const formattedTime = `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
+
+
   return (
     <svg
       className="loading"
@@ -59,20 +83,10 @@ const StoryCard = (props: Props) => {
             d="M0.25,1H0c0,0,0-0.659,0-0.916c0.083-0.303,0.158,0.334,0.25,0C0.25,0.327,0.25,1,0.25,1z"
           />
         </pattern>
-
         <circle id="circle" cx="25%" cy="30%" r="30" />
         <clipPath id="circle-clip">
           <use xlinkHref="#circle" />
         </clipPath>
-
-        {/* <text
-          id="text"
-          transform="matrix(1 0 0 1 -8.0684 116.7852)"
-          fontSize="100px"
-        >
-          CHAEUM
-        </text> */}
-        {/* <Logo id="text"></Logo> */}
         <Active id="storycard"></Active>
 
         <mask id="active-text_mask2">
@@ -105,17 +119,16 @@ const StoryCard = (props: Props) => {
       </g>
 
       {/* <image href="../chacha2.png" x="0%" y="5%" width={50} height={50} /> */}
-      <text x="50%" y="20%" fontSize="1.5em">
+      <text x="42%" y="20%" fontSize="1.1em" fontWeight="bold">
         {props.nickname}
       </text>
-      <text x="50%" y="30%" fontSize="1em">
-        {props.ment}
-      </text>
-      <text x="50%" y="40%" fontSize="1em">
-        {props.tag}
-      </text>
-      <text x="50%" y="70%" fontSize="2em">
-        {props.time}
+      {props.tag.map((tag, index) => (
+        <text key={index} x="42%" y={`${30 + index * 10}%`} fontSize="0.8em">
+          {tag}
+        </text>
+      ))}
+      <text x="42%" y="70%" fontSize="1.5em">
+        {formattedTime}
       </text>
     </svg>
   );
