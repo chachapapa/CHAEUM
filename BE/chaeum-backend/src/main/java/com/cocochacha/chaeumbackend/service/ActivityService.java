baa23feb404e827482a120e860fd07e2714307eb
Kim EuiNyun <kimeuinyun1@gmail.com>
2023-08-07 13:48:52 +0900

    :sparkles: Feat : #220 활동 종료 주석 작성 및 오류 수정
    - 기존에 streak은 임의의 값을 사용했는데, streak table과 매핑하는
      것으로 수정했습니다.

--- BE/chaeum-backend/src/main/java/com/cocochacha/chaeumbackend/service/ActivityService.java
+++ BE/chaeum-backend/src/main/java/com/cocochacha/chaeumbackend/service/ActivityService.java
@@ -1,11 +1,14 @@
 package com.cocochacha.chaeumbackend.service;
 
 import com.cocochacha.chaeumbackend.domain.Activity;
+import com.cocochacha.chaeumbackend.domain.Streak;
 import com.cocochacha.chaeumbackend.dto.AddActivityRequest;
 import com.cocochacha.chaeumbackend.dto.EndActivityRequest;
 import com.cocochacha.chaeumbackend.repository.ActivityRepository;
 
 import java.util.NoSuchElementException;
+
+import com.cocochacha.chaeumbackend.repository.StreakRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -16,9 +19,15 @@ public class ActivityService {
 
     @Autowired
     private final ActivityRepository activityRepository;
+    @Autowired
+    private final StreakRepository streakRepository;
+
     public void createActivity(AddActivityRequest addActivityRequest) {
+        // streakId 가져오기
+        Streak streakId = streakRepository.findById(addActivityRequest.getStreakId()).orElse(null);
+
         Activity activity = Activity.builder()
-                .streakId(addActivityRequest.getStreakId())
+                .streakId(streakId)
                 .build();
 
         activity.changeStartTime(addActivityRequest.getDate());
@@ -44,7 +53,13 @@ public class ActivityService {
             throw new NullPointerException("이미 있는 값");
         } else {
             // null 값이 아닌 경우 종료에 해당하는 값 넣어주기
-            activity.changeStreakId(endActivityRequest.getStreakId());
+            Streak streakId = streakRepository.findById(endActivityRequest.getStreakId()).orElse(null);
+
+            if (streakId == null) {
+                throw new NullPointerException("없는 값!");
+            }
+
+            activity.changeStreakId(streakId);
 
             // 값 저장해주기 => DB에 반영
             activityRepository.updateEnd(endActivityRequest.getEndTime(),
