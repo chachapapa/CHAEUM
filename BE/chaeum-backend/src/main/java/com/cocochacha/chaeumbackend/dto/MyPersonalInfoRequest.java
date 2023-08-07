2c0ebc7a784e395eddb03cbb6d2460d122425f2a
wjdalsdud37 <wjdalsdud37@naver.com>
2023-08-07 15:31:33 +0900

    :bug: Fix : #381 닉네임 변경 관련 버그 수정
    - 유저 정보 초기 설정 요청을 POST -> PATCH로 변경
    - MyPersonalInfoRequest에 @NoArgsConstructor 추가

--- BE/chaeum-backend/src/main/java/com/cocochacha/chaeumbackend/dto/MyPersonalInfoRequest.java
+++ BE/chaeum-backend/src/main/java/com/cocochacha/chaeumbackend/dto/MyPersonalInfoRequest.java
@@ -2,10 +2,12 @@ package com.cocochacha.chaeumbackend.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@NoArgsConstructor
 @AllArgsConstructor
 public class MyPersonalInfoRequest {
 
