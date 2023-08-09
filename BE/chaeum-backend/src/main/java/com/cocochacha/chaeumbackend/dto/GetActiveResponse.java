bd6d99e075f71d66c1dd7eeb1af83735319e39a3
김선우 <kgw6147@naver.com>
2023-08-09 11:22:28 +0900

    :sparkles: Feat : #387 SNS 스토리 목록 반환 DTO 생성
    - SNS 액티브 목록 반환 DTO GetActiveResponse 생성

---
+++ BE/chaeum-backend/src/main/java/com/cocochacha/chaeumbackend/dto/GetActiveResponse.java
@@ -0,0 +1,9 @@
+package com.cocochacha.chaeumbackend.dto;
+
+public class GetActiveResponse {
+
+    private String friendName;
+    private String activeStartName;
+    private int streakId;
+    private int activityId;
+}
