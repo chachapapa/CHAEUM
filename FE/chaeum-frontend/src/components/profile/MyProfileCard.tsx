4788d7a35693f3c301d137111c8b2043deb2fcae
kjy6135@naver.com <kjy6135@naver.com>
2023-08-09 15:28:38 +0900

    :sparkles: Feat : #260 [FE]마이페이지 최초 화면 구현
    - ChaeumNav 수정 : navigate profile 관련해서 임시 아이디를 넣었습니다. 추후 redux에서 닉네임을 불러오면 됩니다.
    - MyProfileCard 수정 : longest -> longest? 로 수정 => 최대 스트릭을 계산할 수 없을 경우를 생각하여 수정했습니다.
    - ProfilePage 수정 : 프로필 페이지입니다. 기존 복붙 코드를 지우고 코드를 추가 했습니다.
    
    - ButtonApp.tsx 추가: 스트릭/게시글/친구 를 나누는 버튼 그룹을 담는 컴포넌트  입니다.
    - ProfileHeader.tsx 추가 : 헤더에 다른 이미지를 사용하기 위하여 따로 컴포넌트를 만들었습니다.
    - ScreenOne.tsx 추가 : 버튼 그룹 내부 컴포넌트입니다. 스트릭 페이지를 보여줍니다.
    - ScreenTwo.tsx 추가 : 버튼 그룹 내부 컴포넌트입니다. 내가 작성한 게시글을 보여줍니다. 해당 코드는 feed 디렉토리의 ArticleCard.tsx 를 따왔으며, 댓글 작성 및 좋아요 누르기를 제한하기 위하여 따로 제작했습니다. 추후 Props로 Article을 받아 적용시킬 예정입니다.
    - ScreenThree.tsx 추가 : 내 친구 목록 페이지입니다. 검색기능을 추가할 예정입니다.

--- FE/chaeum-frontend/src/components/profile/MyProfileCard.tsx
+++ FE/chaeum-frontend/src/components/profile/MyProfileCard.tsx
@@ -3,12 +3,13 @@ import { Avatar } from '@material-tailwind/react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCircleUser } from '@fortawesome/free-solid-svg-icons';
 import { Tag } from '../common/Tag';
+import ImageUpload from '../common/ImageUpload';
 
 interface ProfileCardPropsType {
   name: string;
   age?: number;
   mbti?: string;
-  longest: number;
+  longest?: number;
   profile?: string;
 }
 
@@ -20,7 +21,7 @@ export const MyProfileCard = ({
   profile,
 }: ProfileCardPropsType) => {
   return (
-    <div className="z-50 grid grid-cols-[1fr_3fr_1fr] grid-rows-2 p-2 w-11/12 justify-self-center self-center rounded-md shadow-[0_0_5px_5px_rgba(208,211,222,0.5)] shadow-chauem-gray-500/50 ">
+    <div className="bg-white z-50 grid grid-cols-[1fr_3fr_1fr] grid-rows-2 p-2 w-11/12 justify-self-center self-center rounded-3xl shadow-[0_0_5px_5px_rgba(208,211,222,0.5)] shadow-chauem-gray-500/50 ">
       <div className="profilearea row-span-2 justify-self-center self-center w-3 ">
         {typeof profile === 'undefined' ? (
           <FontAwesomeIcon
@@ -34,8 +35,11 @@ export const MyProfileCard = ({
       <div className="justify-self-start self-center text-xl font-extrabold  text-chaeum-gray-900">
         <span>{name}</span>
       </div>
+
       <div className=" justify-self-center self-center ">
-        <span className="font-bold  text-chaeum-gray-900">최장스트릭</span>
+        {longest ? (
+          <span className="font-bold  text-chaeum-gray-900">현재 </span>
+        ) : null}
       </div>
       <div className="justify-self-start self-center">
         {typeof mbti === 'undefined' ? null : <Tag tag={mbti} />}
@@ -45,7 +49,9 @@ export const MyProfileCard = ({
         <span className="text-chaeum-blue-500 text-bold text-lg">
           {longest}{' '}
         </span>
-        <span className="text-lg text-bold text-chaeum-gray-900">일</span>
+        {longest ? (
+          <span className="text-lg text-bold text-chaeum-gray-900">일</span>
+        ) : null}
       </div>
     </div>
   );
---
+++ FE/chaeum-frontend/src/components/profile/screen/ScreenTwo.tsx
@@ -0,0 +1,223 @@
+import React, { useState } from 'react';
+import FeedPage from '../../../views/FeedPage';
+import { Article, ColorForSelection } from '../../Types';
+import EncourageMessageDetail from '../../feed/EncourageMessageDetail';
+import EncourageMessageCarousel from '../../feed/EncourageMessageCarousel';
+import CommentInput from '../../feed/CommentInput';
+import CommentList from '../../feed/CommentList';
+
+const ScreenTwo = () => {
+  const example1: Article = {
+    id: 1,
+    user: { nickName: 'chacha', profileImage: '../chacha2.png' },
+    date: 'Mar 16, 2020',
+    dateTime: '2020-03-16',
+    activityInfo: { category: '클라이밍', time: 3, color: 'bg-yellow-300' },
+    likeCount: 5,
+    commentCount: 15,
+    content: '오늘은 컴포넌트를 만들어볼거에요 꺄륵',
+    imageList: [
+      '../chacha1.jpg',
+      '../chacha1.jpg',
+      '../chacha1.jpg',
+      '../chacha1.jpg',
+    ],
+    encourageMessageList: [
+      { user: { nickName: 'coco', profileImage: '코코' }, content: '응원글 1' },
+      { user: { nickName: 'lulu', profileImage: '룰루' }, content: '응원글 2' },
+    ],
+    commentList: [
+      { user: { nickName: 'coco', profileImage: '코코' }, content: '댓글 1' },
+      { user: { nickName: 'lulu', profileImage: '룰루' }, content: '댓글 2' },
+    ],
+  };
+
+  const example2: Article = {
+    id: 2,
+    user: { nickName: 'chacha', profileImage: '../chacha1.jpg' },
+    date: 'Mar 16, 2020',
+    dateTime: '2020-03-16',
+    activityInfo: { category: '클라이밍', time: 3, color: 'bg-blue-300' },
+    likeCount: 5,
+    commentCount: 15,
+    content:
+      '오늘은 컴포넌트를 만들어볼거에요 꺄륵꺄륵 \n 오늘 하나는 만들어 놔야지. \n 타입스크립트도 조금 적용 제대로 해서..',
+    imageList: [
+      '../chacha1.jpg',
+      '../chacha1.jpg',
+      '../chacha1.jpg',
+      '../chacha1.jpg',
+    ],
+    encourageMessageList: [
+      { user: { nickName: 'coco', profileImage: '코코' }, content: '댓글 1' },
+      { user: { nickName: 'lulu', profileImage: '룰루' }, content: '댓글 2' },
+    ],
+    commentList: [
+      { user: { nickName: 'coco', profileImage: '코코' }, content: '댓글 1' },
+      { user: { nickName: 'lulu', profileImage: '룰루' }, content: '댓글 2' },
+    ],
+  };
+
+  const arr: ColorForSelection[] = [
+    { color: 'bg-slate-400', hoverColor: 'hover:bg-slate-500' },
+    { color: 'bg-red-400', hoverColor: 'hover:bg-red-500' },
+    { color: 'bg-orange-400', hoverColor: 'hover:bg-orange-500' },
+    { color: 'bg-amber-400', hoverColor: 'hover:bg-amber-500' },
+    { color: 'bg-yellow-400', hoverColor: 'hover:bg-yellow-500' },
+    { color: 'bg-lime-400', hoverColor: 'hover:bg-lime-500' },
+    { color: 'bg-green-400', hoverColor: 'hover:bg-green-500' },
+    { color: 'bg-emerald-400', hoverColor: 'hover:bg-emerald-500' },
+    { color: 'bg-teal-400', hoverColor: 'hover:bg-teal-500' },
+    { color: 'bg-chaeum-blue-400', hoverColor: 'hover:bg-chaeum-blue-500' },
+    { color: 'bg-cyan-400', hoverColor: 'hover:bg-cyan-500' },
+    { color: 'bg-sky-400', hoverColor: 'hover:bg-sky-500' },
+    { color: 'bg-blue-400', hoverColor: 'hover:bg-blue-500' },
+    { color: 'bg-indigo-400', hoverColor: 'hover:bg-indigo-500' },
+    { color: 'bg-violet-400', hoverColor: 'hover:bg-violet-500' },
+    { color: 'bg-purple-400', hoverColor: 'hover:bg-purple-500' },
+    { color: 'bg-fuchsia-400', hoverColor: 'hover:bg-fuchsia-500' },
+    { color: 'bg-pink-400', hoverColor: 'hover:bg-pink-500' },
+    { color: 'bg-rose-400', hoverColor: 'hover:bg-rose-500' },
+  ];
+  const exampleList = [example1, example2];
+
+  const [isPlusButtonClicked, setIsPlusButtonClicked] =
+    useState<boolean>(false);
+  const [detailedArticle, setDetailedArticle] = useState<boolean>(false);
+  const [focusedArticle, setFocusedArticle] = useState<number>();
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [isFadingOut, setIsFadingOut] = useState<boolean>(false);
+
+  const onPlusButtonClicked = (id: number) => {
+    setIsPlusButtonClicked(!isPlusButtonClicked);
+    setFocusedArticle(id);
+  };
+
+  const onMoreCommentClicked = (id: number) => {
+    setDetailedArticle(true);
+    setFocusedArticle(id);
+  };
+
+  const onCloseButtonClicked = (id: number) => {
+    setDetailedArticle(false);
+    setFocusedArticle(id);
+  };
+  return (
+    <div className="h-96 overflow-y-auto">
+      <div>최신순으로 내가 작성한 게시글을 보여줍니다.</div>
+      <div>마이페이지를 누를때마다 서버에서 내 게시글을 가져옵니다.</div>
+      <div>ProfilePage에서 변수로 가지고 있고, </div>
+      <div>ButtonApp에서 Props로 받아옵니다.</div>
+      <div>게시글이 없을때의 예외처리도 생각합니다.</div>
+
+      <div className="max-w-7xl">
+        <div className="w-full">
+          {exampleList.map(post => (
+            <article
+              key={post.id}
+              className="flex p-3 w-full flex-col items-start justify-between mb-3 bg-white"
+            >
+              <div className="flex justify-between w-full">
+                <div className="relative flex items-center gap-x-4">
+                  <img
+                    src={post.user.profileImage}
+                    alt=""
+                    className="h-16 w-16 rounded-full bg-gray-50"
+                  />
+                  <div className="text-lg leading-6">
+                    <p className="text-chaeum-gray-900 text-left">
+                      {post.user.nickName}
+                    </p>
+                    <div
+                      className={`text-sm ${post.activityInfo.color} rounded-md py-0.5 px-1 w-fit`}
+                    >
+                      <p className="text-white text-left">
+                        #{post.activityInfo.category}
+                      </p>
+                      <p className="text-white text-left">
+                        {post.activityInfo.time}시간
+                      </p>
+                    </div>
+                  </div>
+                </div>
+
+                <div>
+                  <time
+                    dateTime={post.dateTime}
+                    className="text-chaeum-gray-900"
+                  >
+                    {post.date}
+                  </time>
+
+                  <div className="text-right text-chaeum-gray-900">
+                    <i className="fa-regular fa-heart mr-0.5 " />
+                    {post.likeCount}
+                    <i className="fa-regular fa-comment ml-2 mr-0.5" />
+                    {post.commentCount}
+                  </div>
+                </div>
+              </div>
+              <div className="group relative">
+                <p className="mt-5 line-clamp-3 text-sm text-left leading-6 text-chaeum-gray-900 whitespace-pre-line">
+                  {post.content}
+                </p>
+              </div>
+              {/* 이미지 미리보기 / 상세 */}
+              {detailedArticle && focusedArticle === post.id ? (
+                <div className="my-5 flex flex-row overflow-auto snap-x">
+                  {post.imageList.map((image, key) => (
+                    <img
+                      src={image}
+                      key={key}
+                      alt=""
+                      className="flex-none mr-2 h-[330px] w-[330px] rounded-lg snap-center"
+                    />
+                  ))}
+                </div>
+              ) : (
+                <div className="mt-5 flex flex-row overflow-auto mb-5">
+                  {post.imageList.map((image, key) => (
+                    <img
+                      src={image}
+                      key={key}
+                      alt=""
+                      className="flex-none mr-2 h-[150px] w-[150px] rounded-lg"
+                    />
+                  ))}
+                </div>
+              )}
+
+              {/* 응원글 미리보기 / 상세 */}
+              {isPlusButtonClicked && focusedArticle === post.id ? (
+                <EncourageMessageDetail
+                  onPlusButtonClicked={() => onPlusButtonClicked(post.id)}
+                  articleId={post.id}
+                />
+              ) : (
+                <EncourageMessageCarousel
+                  onPlusButtonClicked={() => onPlusButtonClicked(post.id)}
+                  articleId={post.id}
+                />
+              )}
+              {/* <CommentInput /> */}
+              {detailedArticle && focusedArticle === post.id ? (
+                <div className="flex flex-col justify-start">
+                  <span onClick={() => onCloseButtonClicked(post.id)}>
+                    닫기
+                  </span>
+                  <CommentList />
+                </div>
+              ) : (
+                <span onClick={() => onMoreCommentClicked(post.id)}>
+                  게시글 상세보기
+                </span>
+              )}
+            </article>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ScreenTwo;
